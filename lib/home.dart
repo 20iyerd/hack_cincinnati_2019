24bc4275df0c2713406ffdc2baea1ecd7d0355f0
unknown <priya.akka12@gmail.com>
2019-07-20 18:01:50 -0400

    submit button

--- lib/home.dart
+++ lib/home.dart
@@ -5,34 +5,34 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to CouchPolitics'),
-        ),
-        body: Center(
-          child:
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(
-                'Enter ZIP Code Here:',
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                  fontSize: 32,
-                  color: Colors.black87,
-                ),
-              ),
-          TextFormField(
-            decoration: InputDecoration(
-                labelText: 'Search for your ZIP Code'
+      appBar: AppBar(
+        title: Text('Welcome to CouchPolitics'),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            'Enter ZIP Code Here:',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 32,
+              color: Colors.black87,
             ),
-          )
-
-            ],
-          )
           ),
-
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Search for your ZIP Code'),
+          ),
+          FlatButton(
+            child: Text('next'),
+            onPressed: () {
+              Navigator.of(context).pushNamed('/listIcons'); //TODO: FIX!
+            },
+            color: Colors.transparent,
+          ),
+        ],
+      )),
     );
   }
 }
