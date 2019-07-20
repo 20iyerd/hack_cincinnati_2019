48df6edc97e7017b780c4a9aae9efb6a2c7a385c
unknown <priya.akka12@gmail.com>
2019-07-20 15:15:56 -0400

    added tabs and tiles

--- lib/list.dart
+++ lib/list.dart
@@ -11,3 +11,67 @@ class _ListPageState extends State<ListPage> {
     return null;
   }
 }
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TabBarDemo());
+}
+
+class TabBarDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(Text('Local')),
+                Tab(Text('State')),
+                Tab(Text('National')),
+              ],
+            ),
+            title: Text('Levels'),
+          ),
+          body: TabBarView(
+            children: [
+              Widget _buildList() => ListView(
+            children: [
+              -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+          -tile(Abortion, Icons.),
+
+
+            ]
+        )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
