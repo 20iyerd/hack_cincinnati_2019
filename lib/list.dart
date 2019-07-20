c1fe255c2f6fec6a05345864b549529b789a04e9
Dee <35465486+20iyerd@users.noreply.github.com>
2019-07-20 14:42:52 -0400

    added fetch post functionality

--- lib/list.dart
+++ lib/list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'post.dart';
 
 class ListPage extends StatefulWidget {
   _ListPageState createState() => _ListPageState();
@@ -11,7 +13,34 @@ class _ListPageState extends State<ListPage> {
     // TODO: implement build
     return Column(
       children: <Widget>[
+
+        FutureBuilder<Post>(
+        future: fetchPost(),
+        builder: (context, snapshot) {
+        if (snapshot.hasData) {
+        return Text(snapshot.data.title);
+        } else if (snapshot.hasError) {
+        return Text("${snapshot.error}");
+        }
+
+        // By default, show a loading spinner.
+        return CircularProgressIndicator();
+        },
+        )
       ],
     );
+  }
+
+  Future<Post> fetchPost() async {
+    final response =
+    await http.get('https://jsonplaceholder.typicode.com/posts/1');
+
+    if (response.statusCode == 200) {
+      // If server returns an OK response, parse the JSON.
+      return Post.fromJson(json.decode(response.body));
+    } else {
+      // If that response was not OK, throw an error.
+      throw Exception('Failed to load post');
+    }
   }
 }
--- lib/post.dart
+++ lib/post.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Post{
@@ -18,16 +17,5 @@ class Post{
       );
 
   }
-  Future<Post> fetchPost() async {
-    final response =
-    await http.get('https://jsonplaceholder.typicode.com/posts/1');
 
-    if (response.statusCode == 200) {
-      // If server returns an OK response, parse the JSON.
-      return Post.fromJson(json.decode(response.body));
-    } else {
-      // If that response was not OK, throw an error.
-      throw Exception('Failed to load post');
-    }
-  }
 }
