58151ae8b010e76ddc2da745cddd3f8238b4a413
Dee <35465486+20iyerd@users.noreply.github.com>
2019-07-20 23:00:01 -0400

    no errors in home

--- lib/home.dart
+++ lib/home.dart
@@ -6,7 +6,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    final Color backgroundColor: white,
+    backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text('Welcome to CouchPolitics!'),
       ),
